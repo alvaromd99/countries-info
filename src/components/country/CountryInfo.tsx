7e0dd9428ec57a2f34db0c7f01c24ab7e7885799
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-10-23 16:25:05 +0200

    add info

--- src/components/country/CountryInfo.tsx
+++ src/components/country/CountryInfo.tsx
@@ -1,6 +1,7 @@
 import { CountryTest } from '../../types/typesTest'
 import './CountryInfo.css'
 import arrow from '../../assets/arrowLeft.svg'
+import Information from '../Info/Information'
 
 interface CountryInfoProps {
 	country: CountryTest
@@ -11,6 +12,8 @@ export default function CountryInfo({
 	country,
 	changeSelected,
 }: CountryInfoProps) {
+	const languagesArr = country.languages.map((lang) => lang.name)
+
 	return (
 		<div className='info-cont'>
 			<div className='btn-cont'>
@@ -25,6 +28,33 @@ export default function CountryInfo({
 				</div>
 				<div className='info-text'>
 					<h2>{country.name}</h2>
+					<div className='info-col-cont'>
+						<div className='col1'>
+							<Information info='Native Name' value={country.nativeName} />
+							<Information
+								info='Population'
+								value={Intl.NumberFormat('en-US')
+									.format(country.population)
+									.toString()}
+							/>
+							<Information info='Region' value={country.region} />
+							<Information info='Sub Region' value={country.subregion} />
+							<Information info='Capital' value={country.capital} />
+						</div>
+						<div className='col2'>
+							<Information
+								info='Top Level Domain'
+								value={country.topLevelDomain[0]}
+							/>
+							{country.currencies && (
+								<Information
+									info='Currencies'
+									value={country.currencies[0].name}
+								/>
+							)}
+							<Information info='Languages' value={languagesArr.join(', ')} />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
