08f4c191d49c5f6a82eebc10e6e90e1b4277963e
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-10-24 16:30:06 +0200

    make dark mode main page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,32 @@
 import './Header.css'
 import moonSvg from '../../assets/moon.svg'
+import sunSvg from '../../assets/sun.svg'
+import { useState } from 'react'
 
 export default function Header() {
+	const [isDarkMode, setIsDarkMode] = useState(false)
+
+	const setDarkMode = () => {
+		document.querySelector('body')?.setAttribute('data-theme', 'dark')
+	}
+
+	const setLightMode = () => {
+		document.querySelector('body')?.setAttribute('data-theme', 'light')
+	}
+
+	const toggleTheme = () => {
+		if (isDarkMode) setLightMode()
+		else setDarkMode()
+
+		setIsDarkMode(!isDarkMode)
+	}
+
 	return (
 		<div className='header'>
 			<h1>Where in the world ?</h1>
-			<button className='theme-btn'>
-				<img src={moonSvg} alt='Moon svg' />
-				<p>Dark mode</p>
+			<button className='theme-btn' onClick={toggleTheme}>
+				<img src={isDarkMode ? sunSvg : moonSvg} alt='Moon svg' />
+				<p>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</p>
 			</button>
 		</div>
 	)
